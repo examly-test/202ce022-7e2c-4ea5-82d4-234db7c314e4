e4d038cc69f6bb8ec0b043f9fccdf1ff666071b8
akashN01 <akash@ontoborn.com>
2022-10-11 13:56:02 +0000

    Enter commit message

--- app.js
+++ app.js
@@ -9,6 +9,3 @@ console.log(`The square root of ${number1} is ${result1}`);
 console.log(`The square root of ${number2} is ${result2}`);
 console.log(`The square root of ${number3} is ${result3}`);console.log(`The square root of ${number1} is ${result1}`);
 console.log(`The square root of ${number2} is ${result2}`);
-console.log(`The square root of ${number3} is ${result3}`);console.log(`The square root of ${number1} is ${result1}`);
-console.log(`The square root of ${number2} is ${result2}`);
-console.log(`The square root of ${number3} is ${result3}`);
