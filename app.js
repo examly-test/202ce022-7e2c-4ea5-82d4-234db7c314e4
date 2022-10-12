7778b5322cde6017908dd1f2d1447134d4760c8b
akashN01 <akash@ontoborn.com>
2022-10-12 09:26:57 +0000

    Backup Branch before deleting

---
+++ app.js
@@ -0,0 +1,11 @@
+const number1 = 2.25;
+const number2 = -4;
+const number3 = 'hello';
+
+console.log(`The square root of ${number1} is ${result1}`);
+console.log(`The square root of ${number2} is ${result2}`);
+console.log(`The square root of ${number3} is ${result3}`);
+console.log(`The square root of ${number1} is ${result1}`);
+console.log(`The square root of ${number2} is ${result2}`);
+console.log(`The square root of ${number3} is ${result3}`);console.log(`The square root of ${number1} is ${result1}`);
+console.log(`The square root of ${number2} is ${result2}`);
